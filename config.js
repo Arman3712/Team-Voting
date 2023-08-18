681e4a844b02ee73145e9117c7d5b0e9b340af3a
Art_of_coding <115411268+Arman3712@users.noreply.github.com>
2023-08-18 22:44:19 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
+//SDK Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyD-QCx3Ycsd9fpGAHt-PBUkikITbjXH-Bg",
+    authDomain: "e-library-8cf3a.firebaseapp.com",
+    projectId: "e-library-8cf3a",
+    storageBucket: "e-library-8cf3a.appspot.com",
+    messagingSenderId: "872028053399",
+    appId: "1:872028053399:web:2154fc22f37994beea2ed2"
+  };
 
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
